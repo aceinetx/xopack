f143903d0ac4935f40d12d37332e488cd28d4e2e
aceinetx <mouselin82@gmail.com>
2026-09-21 15:51:43 +0300

    proper cli

--- src/root.zig
+++ src/root.zig
@@ -10,15 +10,22 @@ fn emitter(userdata: *anyopaque, io: std.Io, filename: [:0]const u8, x: i32, y: 
 }
 
 pub fn main(init: std.process.Init) !void {
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+    if (args.len <= 3) {
+        std.log.err("not enough arguments", .{});
+        std.log.err("usage: {s} [output spritesheet] [scale] [input images...]", .{args[0]});
+        return;
+    }
+
     var packer = Packer.init(init.gpa);
+    defer packer.deinit();
+
     try packer.addInput(.{
-        .filenames = &.{ "lionbee.png", "shit.png", "yt.png", "yt.png", "yt.png", "yt.png" },
-        .output = "output.png",
-        .scale = 1.17,
+        .filenames = args[3..],
+        .output = args[1],
+        .scale = try std.fmt.parseFloat(f32, args[2]),
     });
 
     try packer.packWithEmitter(init.io, emitter, undefined);
     std.debug.print("rects: {}\n", .{i});
-
-    defer packer.deinit();
 }
