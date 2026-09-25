6f8f2a5adbab47c7bdedd5f065935bc2b9540e7b
aceinetx <mouselin82@gmail.com>
2026-09-25 22:55:58 +0300

    cool ass cli

--- src/root.zig
+++ src/root.zig
@@ -2,30 +2,72 @@ const std = @import("std");
 const builtin = @import("builtin");
 const Packer = @import("xopack").Packer;
 
-var i: usize = 0;
+const ArgsVector = []const [:0]const u8;
 
-fn emitter(userdata: *anyopaque, io: std.Io, filename: [:0]const u8, x: i32, y: i32, width: i32, height: i32) void {
-    _ = .{ userdata, io, filename, x, y, width, height };
-    i += 1;
+const ShiftError = error{InvalidArguments};
+
+fn shiftArgs(args: *ArgsVector, message: []const u8) ShiftError![:0]const u8 {
+    if (args.len == 0) {
+        if (message.len > 0) {
+            std.log.err("{s}", .{message});
+            std.log.err("usage        : xopack {{[output spritesheet] [width] [height] [scale] [input images...] +}}", .{});
+            std.log.err("usage example: xopack out1.png 1.0 1024 1024 a.png b.png + out2.png 1.0 1024 1024 b.png a.png", .{});
+        }
+        return ShiftError.InvalidArguments;
+    }
+
+    const arg = args.*[0];
+    args.len -= 1;
+    args.ptr += 1;
+
+    return arg;
 }
 
 pub fn main(init: std.process.Init) !void {
-    const args = try init.minimal.args.toSlice(init.arena.allocator());
-    if (args.len <= 3) {
-        std.log.err("not enough arguments", .{});
-        std.log.err("usage: {s} [output spritesheet] [scale] [input images...]", .{args[0]});
-        return;
-    }
+    var args = try init.minimal.args.toSlice(init.arena.allocator());
+
+    _ = try shiftArgs(&args, "");
+
+    // ----------------------------------------------------------
 
     var packer = Packer.init(init.gpa);
     defer packer.deinit();
 
-    try packer.addInput(.{
-        .filenames = args[3..],
-        .output = args[1],
-        .scale = try std.fmt.parseFloat(f32, args[2]),
-    });
+    // ----------------------------------------------------------
 
-    try packer.packWithEmitter(init.io, emitter, undefined);
-    std.debug.print("rects: {}\n", .{i});
+    var continue_parse = true;
+    while (continue_parse) {
+        const output = try shiftArgs(&args, "no output filename provided");
+        const scale_str = try shiftArgs(&args, "no scale provided");
+        const scale = try std.fmt.parseFloat(f32, scale_str);
+        const width_str = try shiftArgs(&args, "no width provided");
+        const width = try std.fmt.parseInt(i32, width_str, 10);
+        const height_str = try shiftArgs(&args, "no height provided");
+        const height = try std.fmt.parseInt(i32, height_str, 10);
+        var filenames: std.ArrayList([:0]const u8) = try .initCapacity(init.arena.allocator(), 32);
+
+        while (true) {
+            const input = shiftArgs(&args, "") catch {
+                continue_parse = false;
+                break;
+            };
+            if (std.mem.eql(u8, input, "+")) {
+                break;
+            }
+            try filenames.append(init.arena.allocator(), input);
+        }
+
+        const input: Packer.Input = .{
+            .filenames = filenames.items,
+            .output = output,
+            .width = width,
+            .height = height,
+            .scale = scale,
+        };
+        try packer.addInput(input);
+    }
+
+    // ----------------------------------------------------------
+
+    try packer.pack(init.io);
 }
---
+++ testing.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+zig build run -- \
+	output.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output1.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output2.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output3.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output4.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output5.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output6.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output7.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output8.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output9.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output10.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output11.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output12.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output13.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output14.png 0.1 1024 1024 shit.png lionbee.png yt.png + \
+	output15.png 0.1 1024 1024 shit.png lionbee.png yt.png \
